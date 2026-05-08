91b4adbb492e8e248f9fe1b1be4fbce959f73de1
Arslan Telarević <arslan.telarevic@fet.ba>
2026-05-08 16:32:24 +0200

    Definisan model ManufacturerBase.

--- models_b.py
+++ models_b.py
@@ -3,3 +3,12 @@ from typing import Optional
 
 # TODO: Student B - Definiši svoj SQLModel entitet ovdje
 # 
+class ManufacturerBase(SQLModel):
+    name: str
+    country: str
+    founded_year: int
+    is_active: bool
+    annual_revenue: float
+    website: Optional[str] = None
+    employees: int  
+
