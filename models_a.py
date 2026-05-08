9a6ff2a4bf8008b86ccad6f29d227751e474fc48
Tarik Jukan <tarik.jukan@fet.ba>
2026-05-08 17:58:48 +0200

    feat: Kreirana klasa CarBase sa atributima potrebnim prilikom kreiranja nogo auta

--- models_a.py
+++ models_a.py
@@ -3,3 +3,14 @@ from typing import Optional
 
 # TODO: Student A - Definiši svoj SQLModel entitet ovdje
 # 
+
+class CarBase(SQLModel):
+    model_name: str
+    year: int
+    price: float
+    is_electric: bool
+    mileage: int
+    color: str
+    description: Optional[str] = None
+    manufacturer_id: int = Field(default=1, foreign_key="manufacturer.id")
+
