ccb241f1c36c0711a89589c46a9372c97ce5a380
Tarik Jukan <tarik.jukan@fet.ba>
2026-05-09 13:54:46 +0200

    fix: Sitna prepravka koda u routes_a.py

--- routes_a.py
+++ routes_a.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlmodel import Session, select
 from typing import List, Optional
 
